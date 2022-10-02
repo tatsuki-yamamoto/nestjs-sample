235899884312e1776e03702e44b49cce1de2fa8b
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-10-02 14:57:55 +0900

    feat: GraphQLとApolloを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      // schemaファイルのパスを指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // 生成されたschemaを自動でsortされるためのオプションをオンにする
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
